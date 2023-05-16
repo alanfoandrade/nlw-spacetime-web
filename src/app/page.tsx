e3cfe3226e5eea2e8db08643dd1d173c78dd22c2
Alan Andrade <alan@fasters.com.br>
2023-05-16 11:05:42 -0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </main>
+  )
 }
